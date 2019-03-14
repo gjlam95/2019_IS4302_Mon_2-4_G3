b18244803ee767145679df0f0e9576ee88120da2
gjlam95 <gj_lam@hotmail.com>
2019-03-14 16:08:13 +0800

    edit login function

--- client/src/user/login/Login.js
+++ client/src/user/login/Login.js
@@ -49,135 +49,6 @@ class LoginForm extends Component {
         );
     }
 
-    startConnection() {
-       valueRecArray = [];
-       let context = this;
-       var ivStr;
-       this.setState({isLoading:true});
-       navigator.bluetooth.requestDevice({
-         filters: [ {services:[0x2220]}]
-       })
-         .then(device => {
-           deviceConnected = device;
-           context.setState({loading:true});
-           return device.gatt.connect();
-         })
-         .then(server => {
-           return server.getPrimaryService(0x2220);
-         })
-         .then(service => {
-           return service.getCharacteristics();
-         })
-         .then(charArray => {
-           for (let char of charArray) {
-             if (char.properties.write === true && char.uuid.startsWith(writeUid)) {
-               writeChar = char;
-             }
-             if (char.properties.read === true && char.uuid.startsWith(readUid)) {
-               readChar = char;
-             }
-             if (char.uuid.startsWith(disconUid)) {
-               disconnectChar = char;
-             }
-           }
-           const loginRequest = {
-               nric: context.state.nric.value,
-               password: context.state.password.value,
-               role: context.state.role.value,
-           };
-           getServerSignature(loginRequest)
-           .then(response => {
-               ivStr = response.iv;
-               let combined = convertBase64StrToUint8Array(response.combined);
-               let signature = convertBase64StrToUint8Array(response.signature);
-               let iv = convertBase64StrToUint8Array(ivStr);
-               let stringEnder = encoder.encode("//");
-               let sendMsg = concatenate(Uint8Array, combined, signature, stringEnder);
-               let numOfChunks = Math.ceil(sendMsg.byteLength / 20);
-               var msgChunks = splitByMaxLength(sendMsg, numOfChunks);
-               var prevPromise = Promise.resolve();
-               for (let i=0; i< numOfChunks; i++) {
-                  prevPromise = prevPromise.then(function() {
-                    return writeChar.writeValue(msgChunks[i]).then(function() {
-                      if (i === numOfChunks-1) {
-                        wait(11000);
-                          var prevWhilePromise = Promise.resolve();
-                          for (let j=0; j< 7; j++) {
-                             prevWhilePromise = prevWhilePromise.then(function() {
-                               return readChar.readValue().then(value => {
-                                 let valueRec = new Uint8Array(value.buffer);
-                                 if (valueRec[0]===48 && valueRec[1]===48 && j===0) {
-                                   context.setState({isLoading: false});
-                                   dis(disconnectChar);
-                                   openNotificationError(0);
-                                 }
-                                 if (valueRec[0]===33 && valueRec[1]===33) {
-                                   context.setState({isLoading: false});
-                                   dis(disconnectChar);
-                                   openNotificationError(1);
-                                 }
-                                 for (let i=0; i<value.buffer.byteLength; i++) {
-                                   valueRecArray.push(valueRec[i]);
-                                 }
-                                 let ack = "ACK" + j;
-                                 ack = encoder.encode(ack);
-                                 return writeChar.writeValue(ack).then(function() {
-                                   if (j===6) {
-                                     dis(disconnectChar);
-                                     let encryptedMsg = getTagSigAndMsg(valueRecArray);
-                                     let ivMsg = {iv: ivStr};
-                                     let reqToSend =  Object.assign({}, encryptedMsg, ivMsg, loginRequest);
-                                     verifyTagSignature(reqToSend)
-                                      .then(response => {
-                                        localStorage.setItem(AUTH_TOKEN, response.sessionId);
-                                        context.setState({isLoading: false});
-                                        context.props.onLogin();
-                                      }).catch(error => {
-                                        context.setState({isLoading: false});
-                                        notification.error({
-                                            message: 'EquiV',
-                                            description: error.message || 'Sorry! Something went wrong. Please try again!'
-                                        });
-                                      })
-                                    }
-                                 })
-                               })
-                             })
-                           }
-                         }
-                      })
-                    }).catch(error => {
-                        context.setState({isLoading: false});
-                        notification.error({
-                            message: 'EquiV',
-                            description: error.message || 'Sorry! Something went wrong. Please try again!'
-                        });
-                    })
-                  }
-                }).catch(error => {
-                    context.setState({isLoading: false});
-                    if(error.status === 401) {
-                        notification.error({
-                            message: 'EquiV',
-                            description: 'Your NRIC/Password/Role is/are incorrect. Please try again!'
-                        });
-                    } else {
-                        notification.error({
-                            message: 'EquiV',
-                            description: error.message || 'Sorry! Something went wrong. Please try again!'
-                        });
-                    }
-                })
-            }).catch(error => {
-               context.setState({isLoading: false});
-               notification.error({
-                   message: 'EquiV',
-                   description: error.message || 'Sorry! Something went wrong. Please try again!'
-               });
-            })
-         }
-
-
     handleInputChange(event, validationFun) {
         const target = event.target;
         const inputName = target.name;
@@ -266,8 +137,7 @@ class LoginForm extends Component {
                     </Select>
                 </FormItem>
                 <FormItem>
-                    <Button type="primary" htmlType="submit" className="login-form-button" disabled={this.isFormInvalid()}>Login without your tag</Button>
-                    <Button type="primary" className="mfa-button" size="large" onClick={this.startConnection.bind(this)} disabled={this.isFormInvalid()}> Connect Your Tag To Log In </Button>
+                    <Button type="primary" htmlType="submit" className="login-form-button" disabled={this.isFormInvalid()}>Login</Button>
                 </FormItem>
             </Form>
             </Spin>
