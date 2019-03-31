bc27bcfedcdd2e004ed2b1c6a48ba0ca511f49bf
gjlam95 <gj_lam@hotmail.com>
2019-04-01 00:19:27 +0800

    update server.js

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ mongoose
 app.use("/evaluator", proxy("localhost:3001/"));
 app.use("/middleman", proxy("localhost:3002/"));
 app.use("/buyer", proxy("localhost:3003/"));
-app.use("/dealer", proxy("localhost:9090/"));
+app.use("/dealer", proxy("localhost:3004/"));
+app.use("/seller", proxy("localhost:3005/"));
 
 //Use Routes
 app.use("/api/items", require("./routes/api/items"));
