c78df584d53316f454b89bcc1a19345a48f1ffcd
Bryan Tan <bryan@rightspot.co>
2019-03-31 21:40:00 +0800

    Working Dealer & Buyer

--- client/src/common/AppHeader.js
+++ client/src/common/AppHeader.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import {
-  Link,
-  withRouter
-} from 'react-router-dom';
-import './AppHeader.css';
-import { Layout, Menu, Dropdown, Icon } from 'antd';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./AppHeader.css";
+import { Layout, Menu, Dropdown, Icon } from "antd";
 
 const Header = Layout.Header;
 
@@ -15,186 +12,201 @@ class AppHeader extends Component {
   }
 
   handleMenuClick({ key }) {
-    if(key === "logout") {
+    if (key === "logout") {
       this.props.onLogout();
     }
   }
 
-    render() {
-        let menuItems;
-        if(this.props.currentUser) {
-          if(this.props.currentUser.role === "evaluator") {
-            menuItems = [
-              <Menu.Item key="/generatedata">
-                <Link to="/generatedata">
-                  <Icon type="bar-chart" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  balanceAmt={this.props.balanceAmt}
-                  handleMenuClick={this.handleMenuClick}/>
-              </Menu.Item>
-            ];
-          } else if(this.props.currentUser.role === "dealer") {
-            menuItems = [
-              <Menu.Item key="/mylistings">
-                <Link to="/mylistings">
-                  <Icon type="medicine-box" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/managebids">
-                <Link to="/managebids">,
-                  <Icon type="edit" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/pay">
-                <Link to="/pay">
-                  <Icon type="swap" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  balanceAmt={this.props.balanceAmt}
-                  handleMenuClick={this.handleMenuClick}/>
-              </Menu.Item>
-            ];
-          } else if(this.props.currentUser.role === "seller") {
-            menuItems = [
-              <Menu.Item key="/mydata">
-                <Link to="/mydata">
-                  <Icon type="reconciliation" theme="outlined" className="nav-icon" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  balanceAmt={this.props.balanceAmt}
-                  handleMenuClick={this.handleMenuClick}/>
-              </Menu.Item>
-            ];
-          } else if(this.props.currentUser.role === "buyer") {
-            menuItems = [
-              <Menu.Item key="/myassets">
-                <Link to="/myassets">
-                  <Icon type="medicine-box" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/pay">
-                <Link to="/pay">
-                  <Icon type="swap" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/rate">
-                <Link to="/rate">
-                  <Icon type="edit" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/listings">
-                <Link to="/listings">
-                  <Icon type="snippets" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  balanceAmt={this.props.balanceAmt}
-                  handleMenuClick={this.handleMenuClick}/>
-              </Menu.Item>
-            ];
-          } else if(this.props.currentUser.role === "middleman") {
-            menuItems = [
-              <Menu.Item key="/logs">
-                <Link to="/logs">
-                  <Icon type="database" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/pay">
-                <Link to="/pay">
-                  <Icon type="swap" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/manageusers">
-                <Link to="/manageusers">
-                  <Icon type="usergroup-add" />
-                </Link>
-              </Menu.Item>,
-              <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  balanceAmt={this.props.balanceAmt}
-                  handleMenuClick={this.handleMenuClick}/>
-              </Menu.Item>
-            ];
-          } else {
-            menuItems = [
-              <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  handleMenuClick={this.handleMenuClick}/>
-              </Menu.Item>
-            ];
-          }
-        } else {
-          menuItems = [
-            <Menu.Item key="/login">
-              <Link to="/login">Login</Link>
-            </Menu.Item>
-          ];
-        }
-
-        return (
-            <Header className="app-header">
-            <div className="container">
-              <div className="app-title" >
-                <Link to="/">EquiV</Link>
-              </div>
-              <Menu
-                className="app-menu"
-                mode="horizontal"
-                selectedKeys={[this.props.location.pathname]}
-                style={{ lineHeight: '64px' }} >
-                  {menuItems}
-              </Menu>
-            </div>
-          </Header>
-        );
+  render() {
+    let menuItems;
+    if (this.props.currentUser) {
+      if (this.props.currentUser.role === "evaluator") {
+        menuItems = [
+          <Menu.Item key="/generatedata">
+            <Link to="/generatedata">
+              <Icon type="bar-chart" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/profile" className="profile-menu">
+            <ProfileDropdownMenu
+              currentUser={this.props.currentUser}
+              balanceAmt={this.props.balanceAmt}
+              handleMenuClick={this.handleMenuClick}
+            />
+          </Menu.Item>
+        ];
+      } else if (this.props.currentUser.role === "dealer") {
+        menuItems = [
+          <Menu.Item key="/myassets">
+            <Link to="/dealerassets">
+              <Icon type="medicine-box" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/mylistings">
+            <Link to="/mylistings">
+              <Icon type="snippets" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/managebids">
+            <Link to="/managebids">
+              ,
+              <Icon type="edit" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/pay">
+            <Link to="/pay">
+              <Icon type="swap" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/profile" className="profile-menu">
+            <ProfileDropdownMenu
+              currentUser={this.props.currentUser}
+              balanceAmt={this.props.balanceAmt}
+              handleMenuClick={this.handleMenuClick}
+            />
+          </Menu.Item>
+        ];
+      } else if (this.props.currentUser.role === "seller") {
+        menuItems = [
+          <Menu.Item key="/mydata">
+            <Link to="/mydata">
+              <Icon
+                type="reconciliation"
+                theme="outlined"
+                className="nav-icon"
+              />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/profile" className="profile-menu">
+            <ProfileDropdownMenu
+              currentUser={this.props.currentUser}
+              balanceAmt={this.props.balanceAmt}
+              handleMenuClick={this.handleMenuClick}
+            />
+          </Menu.Item>
+        ];
+      } else if (this.props.currentUser.role === "buyer") {
+        menuItems = [
+          <Menu.Item key="/myassets">
+            <Link to="/myassets">
+              <Icon type="medicine-box" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/pay">
+            <Link to="/pay">
+              <Icon type="swap" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/rate">
+            <Link to="/rate">
+              <Icon type="edit" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/listings">
+            <Link to="/listings">
+              <Icon type="snippets" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/profile" className="profile-menu">
+            <ProfileDropdownMenu
+              currentUser={this.props.currentUser}
+              balanceAmt={this.props.balanceAmt}
+              handleMenuClick={this.handleMenuClick}
+            />
+          </Menu.Item>
+        ];
+      } else if (this.props.currentUser.role === "middleman") {
+        menuItems = [
+          <Menu.Item key="/logs">
+            <Link to="/logs">
+              <Icon type="database" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/pay">
+            <Link to="/pay">
+              <Icon type="swap" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/manageusers">
+            <Link to="/manageusers">
+              <Icon type="usergroup-add" />
+            </Link>
+          </Menu.Item>,
+          <Menu.Item key="/profile" className="profile-menu">
+            <ProfileDropdownMenu
+              currentUser={this.props.currentUser}
+              balanceAmt={this.props.balanceAmt}
+              handleMenuClick={this.handleMenuClick}
+            />
+          </Menu.Item>
+        ];
+      } else {
+        menuItems = [
+          <Menu.Item key="/profile" className="profile-menu">
+            <ProfileDropdownMenu
+              currentUser={this.props.currentUser}
+              handleMenuClick={this.handleMenuClick}
+            />
+          </Menu.Item>
+        ];
       }
+    } else {
+      menuItems = [
+        <Menu.Item key="/login">
+          <Link to="/login">Login</Link>
+        </Menu.Item>
+      ];
+    }
+
+    return (
+      <Header className="app-header">
+        <div className="container">
+          <div className="app-title">
+            <Link to="/">EquiV</Link>
+          </div>
+          <Menu
+            className="app-menu"
+            mode="horizontal"
+            selectedKeys={[this.props.location.pathname]}
+            style={{ lineHeight: "64px" }}
+          >
+            {menuItems}
+          </Menu>
+        </div>
+      </Header>
+    );
+  }
 }
 
 function ProfileDropdownMenu(props) {
-  var dropdownMenu = '';
+  var dropdownMenu = "";
 
   dropdownMenu = (
     <Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
-    <Menu.Item key="user-info" className="dropdown-item" disabled>
-    <div className="user-full-name-info">
-    {props.currentUser.name}
-    </div>
-    <div className="nric-info">
-    {props.currentUser.nric}
-    </div>
-    <div className="balance-amt">
-    Balance: ${props.balanceAmt}
-    </div>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="logout" className="dropdown-item">
-    Logout
-    </Menu.Item>
+      <Menu.Item key="user-info" className="dropdown-item" disabled>
+        <div className="user-full-name-info">{props.currentUser.name}</div>
+        <div className="nric-info">{props.currentUser.nric}</div>
+        <div className="balance-amt">Balance: ${props.balanceAmt}</div>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="logout" className="dropdown-item">
+        Logout
+      </Menu.Item>
     </Menu>
   );
 
   return (
     <Dropdown
-    overlay={dropdownMenu}
-    trigger={['click']}
-    getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
-    <a href="/" className="ant-dropdown-pay">
-    <Icon type="user" className="nav-icon" style={{marginRight: 0}} /> <Icon type="down" />
-    </a>
+      overlay={dropdownMenu}
+      trigger={["click"]}
+      getPopupContainer={() =>
+        document.getElementsByClassName("profile-menu")[0]
+      }
+    >
+      <a href="/" className="ant-dropdown-pay">
+        <Icon type="user" className="nav-icon" style={{ marginRight: 0 }} />{" "}
+        <Icon type="down" />
+      </a>
     </Dropdown>
   );
 }
--- client/src/user/dealer/Mylistings.js
+++ client/src/user/dealer/Mylistings.js
@@ -1,87 +1,110 @@
-import React, { Component } from 'react';
-import { Form, Input, Button, Layout, Table, notification } from 'antd';
-import { dealerViewListings, dealerIncludeOffers } from '../../util/APIUtils';
-import './Mylistings.css';
+import React, { Component } from "react";
+import { Form, Input, Button, Layout, Table, notification } from "antd";
+import { dealerViewListings, dealerIncludeOffers } from "../../util/APIUtils";
+import "./Mylistings.css";
 
 const FormItem = Form.Item;
 
 class Dealer_mylistings extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-          listingId: "",
-          description: "",
-          tableData: []
-      }
-      this.includeOffers = this.includeOffers.bind(this);
-      this.loadAllListings = this.loadAllListings.bind(this);
+    super(props);
+    this.state = {
+      listingId: "",
+      description: "",
+      tableData: [],
+      sellingPrice: ""
+    };
+    this.includeOffers = this.includeOffers.bind(this);
+    this.loadAllListings = this.loadAllListings.bind(this);
   }
 
   loadAllListings() {
-      dealerViewListings()
+    dealerViewListings()
       .then(data => {
-        this.setState({ tableData: data })
+        this.setState({ tableData: data });
       })
       .catch(error => {
-          if(error.status === 404) {
-              this.setState({
-                  notFound: true,
-              });
-          } else {
-              this.setState({
-                  serverError: true,
-              });
-          }
+        if (error.status === 404) {
+          this.setState({
+            notFound: true
+          });
+        } else {
+          this.setState({
+            serverError: true
+          });
+        }
       });
   }
 
   includeOffers(event) {
-    event.preventDefault()
+    event.preventDefault();
     const offers = {
       dealerListing: this.state.listingId,
       dealerOffers: this.state.description,
+      dealerSellingPrice: this.state.sellingPrice
     };
     dealerIncludeOffers(offers)
-    .then(response => {
+      .then(response => {
         notification.success({
-            message: 'EquiV',
-            description: "You've successfully entered your bid!",
+          message: "EquiV",
+          description: "You've successfully entered your bid!"
         });
         window.location.reload();
-    }).catch(error => {
+      })
+      .catch(error => {
         notification.error({
-            message: 'EquiV',
-            description: error.message || 'Sorry! Something went wrong. Please try again!'
+          message: "EquiV",
+          description:
+            error.message || "Sorry! Something went wrong. Please try again!"
         });
-    });
+      });
   }
 
   componentDidMount() {
-      this.loadAllListings();
+    this.loadAllListings();
   }
 
   render() {
-      const { Content } = Layout;
+    const { Content } = Layout;
 
-      const users_columns = [{
-        title: 'Listing',
-        dataIndex: 'listingId',
-      }, {
-        title: 'Dealer Offers',
-        dataIndex: 'dealerOffers',
-      }, {
-        title: 'Edit',
-        render: (text) => {
+    const users_columns = [
+      {
+        title: "Listing",
+        dataIndex: "listingId"
+      },
+      {
+        title: "Dealer Offers",
+        dataIndex: "dealerOffers"
+      },
+      {
+        title: "Dealer Selling Price",
+        dataIndex: "dealerSellingPrice"
+      },
+      {
+        title: "Edit",
+        render: text => {
           return (
             <Form onSubmit={this.includeOffers} className="dealer-offers">
               <FormItem>
                 <Input
-                size="small"
-                value={this.state.description}
-                onChange={event => {
-                  this.setState({listingId: text.listingId})
-                  this.setState({description: event.target.value});
-                }}/>
+                  size="small"
+                  placeholder="Offer"
+                  value={this.state.description}
+                  onChange={event => {
+                    this.setState({ listingId: text.listingId });
+                    this.setState({ description: event.target.value });
+                  }}
+                />
+              </FormItem>
+              <FormItem>
+                <Input
+                  size="small"
+                  placeholder="Selling Price"
+                  value={this.state.sellingPrice}
+                  onChange={event => {
+                    this.setState({ sellingPrice: event.target.value });
+                  }}
+                />
               </FormItem>
               <FormItem>
                 <Button
@@ -89,24 +112,23 @@ class Dealer_mylistings extends Component {
                   htmlType="submit"
                   className="dealer-offers-form-button"
                 >
-                Submit
+                  Submit
                 </Button>
               </FormItem>
             </Form>
-          )
+          );
         }
-      }];
+      }
+    ];
 
-      return (
-            <Layout className="userlayout">
-              <Content>
-                <div className="usertitle">
-                  Listings
-                </div>
-                <Table dataSource={this.state.tableData} columns={users_columns}/>
-              </Content>
-            </Layout>
-      );
+    return (
+      <Layout className="userlayout">
+        <Content>
+          <div className="usertitle">Listings</div>
+          <Table dataSource={this.state.tableData} columns={users_columns} />
+        </Content>
+      </Layout>
+    );
   }
 }
 
