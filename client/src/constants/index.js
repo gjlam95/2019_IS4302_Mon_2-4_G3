d38227e471f479f98162830df082a177ae57829c
gjlam95 <gj_lam@hotmail.com>
2019-04-03 16:03:46 +0800

    remove unused functions

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,18 +1,6 @@
-export const API = '/api';
 export const AUTH_TOKEN = 'auth-token'
-
-export const NAME_MIN_LENGTH = 4;
-export const NAME_MAX_LENGTH = 40;
 
 export const NRIC_LENGTH = 9;
 
-export const EMAIL_MAX_LENGTH = 40;
-
-export const PHONE_LENGTH = 8;
-
-export const POSTALCODE_LENGTH = 6;
-
 export const PASSWORD_MIN_LENGTH = 8;
 export const PASSWORD_MAX_LENGTH = 100;
-
-export const NOTE_CONTENT_MAX_LENGTH = 140;
